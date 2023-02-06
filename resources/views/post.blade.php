8a18031b71562f58133b1af79310fb7a98b70e38
luqman73 <luqmanhazli@gmail.com>
2023-02-06 16:09:54 +0800

    EP7 - Make a Route and Link to it

---
+++ resources/views/post.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My BLog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum, natus. Provident saepe quidem quo. Cumque fugit in illo necessitatibus nobis perspiciatis laborum pariatur a? Eligendi sed est iure facilis eius. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum debitis nostrum, perspiciatis tempora doloribus magnam quibusdam aut repudiandae accusamus est aperiam excepturi placeat tempore provident expedita alias nam? Necessitatibus, neque!</p>
+    </article>
+
+    <a href="/">Go Back</a>
+</body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My BLog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum, natus. Provident saepe quidem quo. Cumque fugit in illo necessitatibus nobis perspiciatis laborum pariatur a? Eligendi sed est iure facilis eius. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum debitis nostrum, perspiciatis tempora doloribus magnam quibusdam aut repudiandae accusamus est aperiam excepturi placeat tempore provident expedita alias nam? Necessitatibus, neque!</p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum, natus. Provident saepe quidem quo. Cumque fugit in illo necessitatibus nobis perspiciatis laborum pariatur a? Eligendi sed est iure facilis eius. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum debitis nostrum, perspiciatis tempora doloribus magnam quibusdam aut repudiandae accusamus est aperiam excepturi placeat tempore provident expedita alias nam? Necessitatibus, neque!</p>
+    </article>
+    <article>
+        <h1><a href="">My Third Post</a></h1>
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum, natus. Provident saepe quidem quo. Cumque fugit in illo necessitatibus nobis perspiciatis laborum pariatur a? Eligendi sed est iure facilis eius. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum debitis nostrum, perspiciatis tempora doloribus magnam quibusdam aut repudiandae accusamus est aperiam excepturi placeat tempore provident expedita alias nam? Necessitatibus, neque!</p>
+    </article>
+</body>
+</html>
