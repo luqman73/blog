09da1cdb074beadd892acfd26eca162cf8d60227
luqman73 <luqmanhazli@gmail.com>
2023-02-11 18:07:44 +0800

    EP12 - Used a Composer package for Post Metadata

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,13 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My BLog</title>
+    <title>My Blog</title>
     <link rel="stylesheet" href="/app.css">
 </head>
 
 <body>
     <article>
-        <?= $post; ?>
+        <h1><?= $post->title; ?></h1>
+
+        <div>
+            <?= $post->body; ?>
+        </div>
     </article>
 
     <a href="/">Go Back</a>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,14 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My BLog</title>
+    <title>My Blog</title>
     <link rel="stylesheet" href="/app.css">
 </head>
 
 <body>
     <?php foreach ($posts as $post) : ?>
         <article>
-            <?= $post; ?>
+            <h1>
+                <a href="/posts/<?= $post->slug; ?>">
+                    <?= $post->title; ?>
+                </a>
+            </h1>
+
+            <div>
+                <?= $post->excerpt; ?>
+            </div>
         </article>
     <?php endforeach; ?>
 </body>
