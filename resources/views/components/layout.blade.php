1e22cba8dfc7814e53f389c662a89a3a18dec2c5
luqman73 <luqmanhazli@gmail.com>
2023-02-13 09:50:00 +0800

    EP15 - Blade layouts applied

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<head>
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+
+<body>
+    {{ $slot }}
+</body>
+</html>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
+@extends('layout')
 
-<body>
+@section('content')
     <article>
         <h1>{{ $post->title }}</h1>
 
@@ -17,6 +9,5 @@
         </div>
     </article>
 
-    <a href="/">Go Back</a>
-</body>
-</html>
+    <a href="/">Go Back</a>    
+@endsection
