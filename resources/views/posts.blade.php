234e1eda662a014af57c73bead1556a80a785e02
luqman73 <luqmanhazli@gmail.com>
2023-02-19 16:47:43 +0800

    EP32 - Blade Components and CSS Grids

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,35 +2,15 @@
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card />
+        @if ($posts->count())
+            <x-post-featured-card :post="$posts[0]" />
 
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card />
-            <x-post-card />
-        </div>
+            @if ($posts->count() > 1)
+                <x-posts-grid :posts="$posts" />
+            @endif                  
+        @else
+            <p class="text-center">No posts yet. Please check back later</p>
+        @endif
 
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card />
-            <x-post-card />
-            <x-post-card />
-        </div>
     </main>
-
-    {{-- @foreach ($posts as $post)
-        <article>
-            <h1>
-                <a href="/posts/{{ $post->slug }}">
-                    {!! $post->title !!}
-                </a>
-            </h1>
-
-            <p>
-                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-            </p>
-
-            <div>
-                {!! $post->excerpt !!}
-            </div>
-        </article>
-    @endforeach --}}
 </x-layout>
